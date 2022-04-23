7f4568ba7312157198c5ecd09774c3b02dac8ece
Kittipong6306021630013 <6306021630013@fitm.kmutnb.ac.th>
2022-04-23 19:07:01 +0700

    commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+const path = require('path')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
@@ -20,19 +20,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
-app.use(
-  session({
-    secret: 'my_super_secret$%@#$#fsdfqew',
-    resave: false,
-    saveUninitialized: false
-  })
-)
-
-
-app.use(express.json(Router))
-
-const Router = require('./routes/route')
+const router = require('./routes/route')
 app.use(router)
 
 app.listen(3000, () => console.log('Server Started'))
